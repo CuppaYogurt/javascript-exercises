b4fcfbc6374f551d994063d4aa7997c42c21194c
CuppaYogurt <154710075+CuppaYogurt@users.noreply.github.com>
2026-01-07 21:50:09 -0500

    Do 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+function sumAll (a, b) {
+    let total = 0;
+    let start;
+    let end;
+
+    if (a < b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+
+    
+    if (typeof start != 'number' || typeof end != 'number') {
+        total = 'ERROR'
+        return total;
+    }
+    else if (start >= 0 && end > 0 && start % 1 === 0 && end % 1 === 0) {
+        for (let i = start ; i <= end ; i++) {
+        total += i;
+        }
+        return total;
+    } else {
+        total = 'ERROR';
+        return total;
+    }
 
 };
 
