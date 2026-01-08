68b37aa58bb312754bea59d42f5de8ad5ff75e49
CuppaYogurt <154710075+CuppaYogurt@users.noreply.github.com>
2026-01-07 19:00:33 -0500

    Do 06 to 08 exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+
+    //let removed = arr.filter(item => (item, remove) ? item != remove : removed += item);
+    //return removed;
+    //The above code only works if it accepted one argument
+
+    //return arr.filter(item => (item != remove))
+    //how to get function to accept multiple parameters
+    return arr.filter ( item => !remove.includes(item) );
+    
 };
 
 // Do not edit below this line
